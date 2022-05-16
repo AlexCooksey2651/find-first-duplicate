4b64807ecf136bbdb970e27df1ee6a6e6f582466
Alex Cooksey <aecooksey2651@gmail.com>
2022-05-16 14:23:19 -0400

    Completed assignment

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,9 +1,23 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (newArray.includes(arr[i])) {
+      return arr[i]
+    } else {
+      newArray.push(arr[i])
+    }
+  }
+  return -1
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log('Expecting: -1');
+  console.log("=>", findFirstDuplicate([]))
+
+  console.log('Expecting: -1');
+  console.log("=>", findFirstDuplicate([-1, 3, 5, -1, 7, 2, 2]))
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
@@ -17,3 +31,13 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// IN OWN WORDS:
+// Detecting if values are unique, or if they already exist. Once a value already exists in the array, return that value. 
+// Can iterate over the array and add it to new array. If the new array already includes a value, return that value. If have looped over entire array without already returning something, return -1. 
+
+// create empty array
+// iterate through length of arrayl if new array includes current value, return value
+// else push into new array
+// if nothing returned, return -1
+
+
